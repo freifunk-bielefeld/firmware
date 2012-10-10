f76d26088681a5633f31f234e31a879355616297
mwarning <mwarning@wal.lan>
2012-10-10 20:15:01 +0200

    add access to the batman-adv transglobal table

--- files/www/private/batman-adv.js
+++ files/www/private/batman-adv.js
@@ -3,7 +3,11 @@ function getIP(span) {
     send("/cgi-bin/misc", { func : "mac2ip", mac : span.innerHTML}, function(text) { setText("msg", text); } );
 }
 
-send("/cgi-bin/batman-adv", { func: "get_originators" }, function(text) {
-    text = text.replace(mac_regex, '<span onclick="getIP(this)" class="mac">$1</span>');
-    setText('originators', text);
-});
+function update(name) {
+    send("/cgi-bin/batman-adv", { func : name }, function(text) {
+        text = text.replace(mac_regex, '<span onclick="getIP(this)" class="mac">$1</
+        setText('output', text);
+    });
+}
+
+update("get_originators");
