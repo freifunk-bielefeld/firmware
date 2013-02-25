4aae4e622b0c72224524c10bd2781c36cdf3e6b4
Moritz Warning <moritzwarning@web.de>
2013-02-25 21:44:39 +0100

    add a simple file manager to change the splash page content

--- files/www/private/nodogsplash.js
+++ files/www/private/nodogsplash.js
@@ -4,16 +4,26 @@ var mac_regex = /([0-9a-f]{1,2}(:[0-9a-f]{1,2}){5})/gi;
 function addIP(span)
 {
 	var mac = span.innerHTML;
-	var ul = get('mac_list');
+	var ul = get('nds_macs');
 
-	onChilds(ul, 'LI', function(e) { if(e.textContent == mac) mac = ""; });
+	onChilds(ul, 'LABEL', function(e) { if(e.textContent == mac) mac = ""; });
 	if(mac.length == 0) return;
 
-	var li = create('li');
-	li.onclick = function() { ul.removeChild(this); };
-	li.appendChild(document.createTextNode(mac));
+	var label = create('LABEL');
+	label.onclick = function() { ul.removeChild(this); };
+	label.appendChild(document.createTextNode(mac));
 
-	ul.appendChild(li);
+	ul.appendChild(label);
+}
+
+function addDeleteButton(div, filename)
+{
+	append_button(div, "L&ouml;schen", function() {
+		send("/cgi-bin/nodogsplash", { func: "delete_file", filename : filename }, function(data) {
+			setText('msg', data);
+			reload();
+		});
+	});
 }
 
 function reload()
@@ -23,12 +33,27 @@ function reload()
 		text = text.replace(mac_regex, '<span onclick="addIP(this)" class="mac">$1</span>');
 		setText('nds_status', text);
 	});
+
+	send("/cgi-bin/nodogsplash", { func: "list_files" }, function(text) {
+		var span = get('nds_files');
+		removeChilds(span);
+		var files = split(text);
+		for(var i in files)
+		{
+			var div = append(span, 'div');
+			var label = append(div, 'label');
+			var a = append(label, 'a');
+			a.href="/cgi-bin/nodogsplash?func=download_file&filename="+files[i];
+			a.innerHTML = files[i];
+			addDeleteButton(div, files[i]);
+		}
+	});
 }
 
 function button_action(func)
 {
 	var macs = "";
-	onChilds(get('mac_list'), 'LI', function(e) { macs += " " + e.textContent; });
+	onChilds(get('nds_macs'), 'LABEL', function(e) { macs += " " + e.textContent; });
 
 	if(macs.length == 0) return;
 	send("/cgi-bin/nodogsplash", { func : func, macs : macs }, function(data) {
