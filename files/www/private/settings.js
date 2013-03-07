7f8e6e0a1d1451c07611f8baf9ad503a74ca1ae3
Moritz Warning <moritzwarning@web.de>
2013-03-07 19:25:07 +0100

    add advanced mode to enable several option to advanced users; like changing the SSID of the mesh interface

--- files/www/private/settings.js
+++ files/www/private/settings.js
@@ -58,7 +58,8 @@ function appendSetting(p, path, value, mode)
 	case "ssid":
 		b = append_input(p, "SSID", id, value);
 		if(mode == "public" || mode == "mesh")
-			b.lastChild.disabled = "disabled";
+			if(!get("adv_mode").checked)
+				b.lastChild.disabled = "disabled";
 		addInputCheck(b.lastChild, /^[^\x00-\x1F\x80-\x9F]{3,30}$/, "SSID ist ung\xfcltig.");
 		break;
 	case "share_internet":
