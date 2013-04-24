be2de8c28e51f62dc38384d683daa1f4a4645712
Moritz Warning <moritzwarning@web.de>
2013-04-24 03:48:35 +0200

    create EUI-64 ip addresses

--- files/sbin/mac2ip6.sh
+++ files/sbin/mac2ip6.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-# Create an IPv6 address based on a given MAC
-# and the configured IPv6 prefix.
-
+# Create an IPv6 address based on a prefix and
+# the 64-bit Extended Unique Identifier (EUI-64).
 
 mac=$1
 prefix=`uci get -q freifunk.@settings[0].ff_prefix`
 
 [ -z "$mac" -o -z "$ff_prefix" -o ${#mac} -ne 17 ] && exit 1
 
-ip="$prefix:"
-i=0
-IFS=':';
-for hex in $mac; do
-	[  $i -ne 0 -a $(($i%2)) -eq 0 ] && {
-		ip="$ip:"
-	}
-	ip="$ip$hex"
-	i=$((i+1))
-done
-IFS="
-"
-echo $ip
+
+#translate to local administered mac
+a=${mac%%:*} #cut out first hex
+a=$((0x$a | 2)) #set second bit to 1
+a=`printf '%02x\n' $a` #convert to hex
+mac="$a:${mac#*:}"
+
+#insert FFEE
+mac=${mac//:/}
+mac=${mac:0:6}FFFE${mac:6:6}
+mac=`echo $mac | sed 's/..../&:/g'`
+
+# assemble ip
+echo "$prefix${mac%?}"
