69aa1be84db55bb9c3a23d915b17b8b8e794cbd6
Moritz Warning <moritzwarning@web.de>
2013-04-24 23:21:13 +0200

    more comments

--- files/sbin/mac2ip6.sh
+++ files/sbin/mac2ip6.sh
@@ -9,16 +9,16 @@ prefix=`uci get -q freifunk.@settings[0].ff_prefix`
 [ -z "$mac" -o -z "$ff_prefix" -o ${#mac} -ne 17 ] && exit 1
 
 
-#translate to local administered mac
-a=${mac%%:*} #cut out first hex
-a=$((0x$a | 2)) #set second bit to 1
-a=`printf '%02x\n' $a` #convert to hex
+# translate to local administered mac
+a=${mac%%:*} # cut out first hex
+a=$((0x$a | 2)) # set second least significant bit to 1
+a=`printf '%02x\n' $a` # convert back to hex
 mac="$a:${mac#*:}"
 
-#insert FFEE
-mac=${mac//:/}
-mac=${mac:0:6}FFFE${mac:6:6}
-mac=`echo $mac | sed 's/..../&:/g'`
+
+mac=${mac//:/} # remove ':'
+mac=${mac:0:6}FFFE${mac:6:6} # insert FFEE
+mac=`echo $mac | sed 's/..../&:/g'` # insert ':'
 
 # assemble ip
 echo "$prefix${mac%?}"
