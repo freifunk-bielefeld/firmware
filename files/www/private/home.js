0007ff0c80ef1c13625804a8dab47ba6e3cff580
Moritz Warning <moritzwarning@web.de>
2014-03-21 06:02:10 +0100

    refactor code and content for home site - also add traffic counters

--- files/www/private/home.js
+++ files/www/private/home.js
@@ -1,7 +1,37 @@
 
-send("/cgi-bin/misc", { func: "status" }, function(data) {
+function formatSize(bytes) {
+	if(typeof bytes === "undefined" || bytes == "") {
+		return "-";
+	} else if (bytes < 1024) {
+		return bytes + "  b ";
+	} else if (bytes < 1024*1024) {
+		return (bytes/ 1024.0).toFixed(0)  + " kB";
+	} else if (bytes < 1024*1024*1024) {
+		return (bytes/1024.0/1024.0).toFixed(1)  + " MB";
+	} else {
+		return (bytes/1024.0/1024.0/1024.0).toFixed(2) + " GB";
+	}
+}
+
+send("/cgi-bin/home", { }, function(data) {
 	var obj = fromUCI(data).misc.data;
-	for(var key in obj)
-		if(key != 'stype')
-			setText(key, obj[key]);
+	for(var key in obj) {
+		var value = obj[key];
+
+		if(key == 'stype') {
+			continue;
+		}
+
+		if(/_bytes$/.test(key)) {
+			value = formatSize(value);
+		}
+
+		setText(key, value);
+	}
 });
+
+addHelpText(get("system"), "Eine \xdcbersicht \xfcber den Router.");
+addHelpText(get("public"), "Das \xf6ffentliche Freifunknetz..");
+addHelpText(get("private"), "Das private Netz bzw. LAN.");
+addHelpText(get("wan"), "Das Netz \xfcber dass das Internet erreicht wird.");
+addHelpText(get("software"), "Einige installierte Softwareversionen.");
