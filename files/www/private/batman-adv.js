d18c597d2934d7b9ac366e7e4958f74e96489e3b
Moritz Warning <moritzwarning@web.de>
2014-08-19 23:17:57 +0200

    remove some dependencies on mac2ip; we want to get rid of this

--- files/www/private/batman-adv.js
+++ files/www/private/batman-adv.js
@@ -1,8 +1,4 @@
 
-function getIP(span) {
-	send("/cgi-bin/misc", { func : "mac2ip", mac : span.innerHTML }, function(text) { setText("msg", text); } );
-}
-
 var regex = {
 	get_originators : /^$/gi, //matches nothing
 	get_transglobal : /(via\s+)([0-9a-f]{1,2}(:[0-9a-f]{1,2}){5})/gi,
@@ -12,7 +8,6 @@ var regex = {
 function update(name) {
 	setText('msg', "");
 	send("/cgi-bin/batman-adv", { func : name }, function(text) {
-		text = text.replace(regex[name], '$1<span onclick="getIP(this)" class="mac">$2</span>');
 		setText('output', text);
 	});
 }
