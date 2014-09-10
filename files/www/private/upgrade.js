41a477e8e1d1ecc2cea0f48def29b48fa0ad89b4
Moritz Warning <moritzwarning@web.de>
2014-09-10 19:56:34 +0200

    add buttons to search for new firmwares and reset the firmware

--- files/www/private/upgrade.js
+++ files/www/private/upgrade.js
@@ -2,3 +2,23 @@
 function init() {
 	/* Nothing to do */
 }
+
+function restore_firmware() {
+	if(!confirm("Sollen alle Einstellungen zur\xFCckgesetzt werden?")) return;
+	send("/cgi-bin/upgrade", { func : 'restore_firmware' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_upgrade() {
+	send("/cgi-bin/upgrade", { func : 'lookup_upgrade' }, function(text) {
+		setText('msg', text);
+	});
+}
+
+function lookup_and_apply_upgrade() {
+	if(!confirm("Soll ein update durchgef\xFChrt werden?")) return;
+	send("/cgi-bin/upgrade", { func : 'lookup_and_apply_upgrade' }, function(msg) {
+		setText('msg', text);
+	});
+}
