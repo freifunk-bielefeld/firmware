4771f87c56b126516d6ebc5e7562cfe6691ec63e
Moritz Warning <moritzwarning@web.de>
2015-11-07 23:11:48 +0100

    fix port mapping for wr941 v6

--- files/www/lan/network.js
+++ files/www/lan/network.js
@@ -640,7 +640,7 @@ function collect_switch_info(device)
 			break;
 		case 'tp-link-tl-wr941n-nd-v6':
 			obj.ifname = "eth1";
-			obj.port_map = [['_',0], ['1',1], ['2',2], ['3',3], ['4',4]];
+			obj.port_map = [['_',0], ['1',4], ['2',3], ['3',2], ['4',1]];
 			break;
 		case 'tp-link-cpe210-v1-0':
 		case 'tp-link-cpe220-v1-0':
