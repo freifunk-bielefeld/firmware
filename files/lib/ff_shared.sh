b7a58f42a05dd67e02b3e2fffe8275460091ad52
Moritz Warning <moritzwarning@web.de>
2016-04-23 20:41:28 +0200

    forgot character

--- files/lib/ff_shared.sh
+++ files/lib/ff_shared.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Create an IPv6 ULA-address.
+#create an IPv6 ULA-address based on EUI-64
 ula_addr()
 {
-	local prefix a mac="$1"
+        local prefix a mac="$1"
 
-	prefix="(uci get network.globals.ula_prefix)"
+	prefix="$(uci get network.globals.ula_prefix)"
 
 	# translate to local administered mac
 	a=${mac%%:*} #cut out first hex
@@ -13,10 +13,10 @@ ula_addr()
 	a=$(printf '%02x\n' $a) #convert back to hex
 	mac="$a:${mac#*:}" #reassemble mac
 
-	mac=${mac//:/} # remove ':'
-	mac=${mac:0:6}fffe${mac:6:6} # insert ffee
-	mac=$(echo $mac | sed 's/..../&:/g') # insert ':'
+        mac=${mac//:/} # remove ':'
+        mac=${mac:0:6}fffe${mac:6:6} # insert ffee
+        mac=$(echo $mac | sed 's/..../&:/g') # insert ':'
 
-	# assemble IPv6 address
-	echo "${prefix%%::*}:${mac%?}"
+        # assemble IPv6 address
+        echo "${prefix%%::*}:${mac%?}"
 }
