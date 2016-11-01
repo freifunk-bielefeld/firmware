cd48b28bb7bd3e39e53e477ecd60522a2e7030c2
Moritz Warning <moritzwarning@web.de>
2016-11-01 02:05:52 +0100

    pass addr prefix to ula_addr

--- files/lib/ff_shared.sh
+++ files/lib/ff_shared.sh
@@ -3,9 +3,7 @@
 #create an IPv6 ULA-address based on EUI-64
 ula_addr()
 {
-	local prefix a mac="$1" invert=${2:-0}
-
-	prefix="$(uci get network.globals.ula_prefix)"
+	local prefix a  prefix="$1" mac="$2" invert=${3:-0}
 
 	if [ $invert -eq 1 ]; then
 		# translate to local administered mac
