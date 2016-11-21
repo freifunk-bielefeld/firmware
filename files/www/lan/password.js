46e6bbbd0bc0084c42c40ff982ec24d714834646
Moritz Warning <moritzwarning@web.de>
2016-11-21 19:34:03 +0100

    use german messages

--- files/www/lan/password.js
+++ files/www/lan/password.js
@@ -12,10 +12,10 @@ function apply()
 	$('p2').value = "";
 
 	if(p1 != p2) {
-		setText('msg', "(E) Die Passw&ouml;rter sind nicht identisch.");
+		setText('msg', "Die Passw&ouml;rter sind nicht identisch.");
 		return;
 	} else {
-		setText('msg', "(I) Das Passwort wird ge&auml;ndert. Bitte die Seite neu laden.");
+		setText('msg', "Das Passwort wird ge&auml;ndert. Bitte die Seite neu laden.");
 	}
 
 	send("/cgi-bin/password", { func : "set_password", pass1 : p1, pass2 : p2 }, function(data) {
