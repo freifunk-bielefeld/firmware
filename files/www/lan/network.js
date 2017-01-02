3ad8feeb269f870bb08cb2a8ad3160a1f398d066
Swen Thümmler <st@jpc.de>
2017-01-02 13:56:10 +0100

    fix switch config for wr1043 v4 another time

--- files/www/lan/network.js
+++ files/www/lan/network.js
@@ -526,7 +526,6 @@ function collect_switch_info(device)
 	{
 		case 'tp-link-tl-wdr3600-v1':
 		case 'tp-link-tl-wdr4300-v1':
-		case 'tp-link-tl-wr1043n-nd-v4':
 			obj.map = [['eth0',0],['WAN',1],['LAN1',2],['LAN2',3],['LAN3',4],['LAN4',5]];
 			break;
 		case 'tp-link-tl-wr1043n-nd-v1':
@@ -535,6 +534,9 @@ function collect_switch_info(device)
 		case 'tp-link-tl-wr1043n-nd-v2':
 		case 'tp-link-tl-wr1043n-nd-v3':
 			obj.map = [['eth1',0],['WAN',5],['LAN1',4],['LAN2',3],['LAN3',2],['LAN4',1]];
+			break;
+		case 'tp-link-tl-wr1043n-nd-v4':
+			obj.map = [['eth0',0],['WAN',5],['LAN1',4],['LAN2',3],['LAN3',2],['LAN4',1]];
 			break;
 		case 'tp-link-tl-wdr3500-v1':
 		case 'tp-link-tl-wr741n-nd-v4':
