c39c27c15a835850c8893548302813a45b6c0213
Moritz Warning <moritzwarning@web.de>
2018-12-12 11:39:55 +0100

    enable wr1043n-v5 switch configuration for web ui

--- files/www/lan/network.js
+++ files/www/lan/network.js
@@ -533,10 +533,8 @@ function collect_switch_info(device)
 		case 'tp-link-tl-wr1043n-nd-v2':
 		case 'tp-link-tl-wr1043n-nd-v3':
 		case 'tp-link-tl-wr1043n-nd-v4':
+		case 'tp-link-tl-wr1043n-v5':
 			obj.map = [['eth1',0],['WAN',5],['LAN1',4],['LAN2',3],['LAN3',2],['LAN4',1]];
-			break;
-		case 'tp-link-tl-wr1043n-nd-v4':
-			obj.map = [['eth0',0],['WAN',5],['LAN1',4],['LAN2',3],['LAN3',2],['LAN4',1]];
 			break;
 		case 'tp-link-tl-wdr3500-v1':
 		case 'tp-link-tl-wr741n-nd-v4':
